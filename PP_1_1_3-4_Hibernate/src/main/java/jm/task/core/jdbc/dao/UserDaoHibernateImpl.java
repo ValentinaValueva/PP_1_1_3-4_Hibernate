715a76584e5d82e61cd49025da6a54950626db0b
Valentina Valueva <arowanaok@gmail.com>
2025-12-23 22:42:41 +0300

    обернула методы в try catch в классе UserDaoHibernateImpl

--- PP_1_1_3-4_Hibernate/src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ PP_1_1_3-4_Hibernate/src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -2,12 +2,14 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 
 import org.hibernate.Transaction;
 
 import java.util.List;
 
+import static org.hibernate.tool.schema.SchemaToolingLogging.LOGGER;
 
 
 public class UserDaoHibernateImpl implements UserDao {
@@ -20,54 +22,76 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void createUsersTable() {
         Session session = Util.getInstance().getSessionFactory().getCurrentSession();
-        Transaction tx = session.beginTransaction();
-        session.createNativeQuery("CREATE TABLE IF NOT EXISTS users (id BIGINT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), lastName VARCHAR(255), age TINYINT)").executeUpdate();
-        tx.commit();
-        System.out.println("Таблица создана");
-    session.close();
+        try {
+            Transaction tx = session.beginTransaction();
+            session.createNativeQuery("CREATE TABLE IF NOT EXISTS users (id BIGINT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), lastName VARCHAR(255), age TINYINT)").executeUpdate();
+            tx.commit();
+            System.out.println("Таблица создана");
+            session.close();
+        } catch (HibernateException e) {
+            LOGGER.error("Ошибка создания таблицы: " + e.getMessage());
+        }
 
     }
+
     @Override
     public void dropUsersTable() {
         Session session = Util.getInstance().getSessionFactory().getCurrentSession();
-        Transaction tx = session.beginTransaction();
-        session.createNativeQuery("DROP TABLE IF EXISTS users").executeUpdate();
-        tx.commit();
-        System.out.println("Таблица удалена");
-        session.close();
+        try {
+            Transaction tx = session.beginTransaction();
+            session.createNativeQuery("DROP TABLE IF EXISTS users").executeUpdate();
+            tx.commit();
+            System.out.println("Таблица удалена");
+            session.close();
+        } catch (HibernateException e) {
+            LOGGER.error("Ошибка удаления таблицы: " + e.getMessage());
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
         Session session = Util.getInstance().getSessionFactory().getCurrentSession();
-        Transaction tx = session.beginTransaction();
-        User user = new User(name, lastName, age);
-        session.save(user);
-        tx.commit();
-        System.out.println("User с именем – " + name + " добавлен в базу данных");
-    session.close();
+        try {
+            Transaction tx = session.beginTransaction();
+            User user = new User(name, lastName, age);
+            session.save(user);
+            tx.commit();
+            System.out.println("User с именем – " + name + " добавлен в базу данных");
+        } catch (Exception e) {
+            LOGGER.error("Ошибка при добавлении User " + e.getMessage());
+        }
+        session.close();
     }
 
     @Override
     public void removeUserById(long id) {
         Session session = Util.getInstance().getSessionFactory().getCurrentSession();
-        Transaction tx = session.beginTransaction();
-        User user = session.get(User.class, id);
-        if (user != null) {
-            session.delete(user);
+        try {
+            Transaction tx = session.beginTransaction();
+            User user = session.get(User.class, id);
+            if (user != null) {
+                session.delete(user);
+            }
+            tx.commit();
+            System.out.println("User c id " + id + " удален");
+        } catch (Exception e) {
+            LOGGER.error("Ошибка при удалении User " + e.getMessage());
         }
-        tx.commit();
-        System.out.println("User c id " + id + " удален");
         session.close();
     }
 
     @Override
     public List<User> getAllUsers() {
         Session session = Util.getInstance().getSessionFactory().getCurrentSession();
-        Transaction tx = session.beginTransaction();
-        List<User> users = session.createQuery("FROM User").getResultList();
-        tx.commit();
-        users.forEach(System.out::println);
+        List<User> users = null;
+        try {
+            Transaction tx = session.beginTransaction();
+            users = session.createQuery("FROM User", User.class).getResultList();
+            tx.commit();
+            users.forEach(System.out::println);
+        } catch (Exception e) {
+            LOGGER.error("Ошибка при удалении User " + e.getMessage());
+        }
         session.close();
         return users;
     }
@@ -75,10 +99,14 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void cleanUsersTable() {
         Session session = Util.getInstance().getSessionFactory().getCurrentSession();
-        Transaction tx = session.beginTransaction();
-        session.createQuery("DELETE FROM User").executeUpdate();
-        tx.commit();
-        System.out.println("Таблица удалена");
+        try {
+            Transaction tx = session.beginTransaction();
+            session.createQuery("DELETE FROM User").executeUpdate();
+            tx.commit();
+            System.out.println("Все записи из таблицы users удалены.");
+        } catch (Exception e) {
+            LOGGER.error("Ошибка очистки таблицы " + e.getMessage());
+        }
         session.close();
 
     }
